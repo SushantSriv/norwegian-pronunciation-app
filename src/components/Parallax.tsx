f8f7b8d2365477c80134416f682869dfacf68e35
Sushant Kumar Srivastava <sushant.kumar.srivastava@aibel.com>
2025-07-15 14:42:42 +0200

    css endring

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -1,16 +1,17 @@
+// src/components/Parallax.tsx
 import React from 'react';
 import Particles from 'react-tsparticles';
 
 import bgFar from '../assets/bg/bg_far.png';
 import bgMid from '../assets/bg/bg_mid.png';
 import bgFore from '../assets/bg/bg_fore.png';
-import snow from '../assets/particles/snowflake.png'; // legg denne i src/assets/particles/
+import snowPNG from '../assets/particles/snowflake.png';
 
 export const Parallax: React.FC = () => (
     <>
-        <img className="bg far" src={bgFar} />
-        <img className="bg mid" src={bgMid} />
-        <img className="bg fore" src={bgFore} />
+        <img className="bg far" src={bgFar} alt="" />
+        <img className="bg mid" src={bgMid} alt="" />
+        /<img className="bg fore" src={bgFore} alt="" />
         <Particles
             id="snow"
             className="pointer-events-none"
@@ -20,9 +21,9 @@ export const Parallax: React.FC = () => (
                     number: { value: 120 },
                     size: { value: { min: 2, max: 5 } },
                     move: { enable: true, speed: 0.3, direction: 'bottom' },
-                    shape: { type: 'image', image: [{ src: snow, width: 32, height: 32 }] },
+                    shape: { type: 'image', image: [{ src: snowPNG, width: 32, height: 32 }] },
                     opacity: { value: { min: 0.3, max: 0.9 } },
-                },
+                }
             }}
         />
     </>
